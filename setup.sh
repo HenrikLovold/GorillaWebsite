9d1dc6e2b5f54b68f92e90d7e0c4695d85189860
Henrik Løvold <henrik.lovold@gmail.com>
2025-06-05 18:28:06 +0000

    Added json cp to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-docker build . -t gorillaserver && docker run -d -p 80:80 --name gorilla-app gorillaserver 
+docker build . -t gorillaserver && docker run -d -p 80:80 --name gorilla-app gorillaserver && docker cp *.json gorilla-app:/var/www/html 
