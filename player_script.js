f663f6ea9a8bb4c5fa7c8dcbb5c6f8e1a4fa9a86
Henrik Løvold <henrik.lovold@gmail.com>
2025-06-10 20:14:06 +0200

    Updated stats

---
+++ player_script.js
@@ -0,0 +1,18 @@
+const displayer = Vue.createApp({
+    data () {
+        return {
+            
+        };
+    },
+    methods: {
+        async main () {
+            
+        },
+    },
+
+    mounted() {
+        this.main();
+    }
+});
+
+displayer.mount("#app-root");
